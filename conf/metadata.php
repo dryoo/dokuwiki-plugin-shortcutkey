9deee2b8d0531dcf766bb1d5fa654756eb5a38d7
Zane Knott <zane.knott@gmail.com>
2016-03-08 20:15:06 -0800

    Add shortcut to save page.

--- conf/metadata.php
+++ conf/metadata.php
@@ -17,4 +17,5 @@ $meta['key_search']=
 $meta['key_show']=
 $meta['key_edit']=
 $meta['key_home']=
+$meta['key_save']=
     array('multichoice','_choices' => array('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','X','T','U','V','W','X','Y','Z'));
